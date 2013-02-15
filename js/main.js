164f6f53bf4569ce8e1caac8252e5196a671b366
Jeff Cordova <cordova.jeffrey@gmail.com>
2013-02-15 14:59:56 +0800

    Highlighting Tapped or Clicked UI Elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 var app = {
 
+    registerEvents: function(){
+        var self = this;
+
+        // Check if browser supports touch events
+        if(document.documentElement.hasOwnProperty('ontouchstart')){
+            $('body').on('touchstart', 'a', function(event){
+                $(event.target).addClass('tappable-active');
+            });
+
+            $('body').on('touchend', 'a', function(event){
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event){
+                console.log('mousedown');
+                $(event.target).addClass('tappable-active');
+            });
+
+            $('body').on('mouseup', 'a', function(event){
+                console.log('mouseup');
+                $(event.target).removeClass('tappable-active');
+            })
+        }
+    },
+
     showAlert: function(message, title){
         if(navigator.notification){
             navigator.notification.alert(message, null, title, 'OK');
@@ -10,6 +36,7 @@ var app = {
 
     initialize: function() {
         var self = this;
+        this.registerEvents();
         this.store = new MemoryStore(function(){
             $('body').html(new HomeView(self.store).render().el);
         });
